674698d37f28859301956c983a5eca4e3d812d3f
Thirumalai Nagalingam <81082960+thiru31@users.noreply.github.com>
2024-04-14 19:37:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int matching(char a, char b) {
+        if (a == '(' && b == ')') {
+            return 1;
+        } else if (a == '{' && b == '}') {
+            return 1;
+        } else if (a == '[' && b == ']') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+        int flag=1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if(s.length()%2==1){
+                flag=0;
+                break;
+            }
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else if (st.empty() || !matching(st.top(),s[i])) {
+                flag =0;
+                break;
+            } else {
+                st.pop();
+            }
+        }
+        return flag;
+    }
+};
