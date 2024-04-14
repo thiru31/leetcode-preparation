ee5bb63447963e160406581ef7880f498eed7927
Thirumalai Nagalingam <81082960+thiru31@users.noreply.github.com>
2024-04-15 00:49:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        stable_partition(nums.begin(), nums.end(), [](int n) { return n > 0; });
+    }
+};
